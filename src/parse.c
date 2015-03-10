a97e40c2994659e31db9f83be6ed4465d65ac677
Chris Hall <chris.hall@catalyst.net.nz>
2015-03-10 15:39:57 +1300

    moving to a table based parser

--- src/parse.c
+++ src/parse.c
@@ -5,18 +5,47 @@
 #include "types.h"
 #include "ast.h"
 #include "parse.h"
+
+#define DEBUG_PARSE 1
+
+#define LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
 /* ignore unused parameter warnings */
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-struct ic_type_decl * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i){
+struct ic_expression * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i){
     /* FIXME */
     return 0;
 }
 
+struct ic_expression * ic_parse_func_decl(struct ic_tokens *tokens, unsigned int *i){
+    /* FIXME */
+    return 0;
+}
+
+/* an entry in the parse table showing the
+ * length and token string that must be match
+ * and if a match is found, the function to dispatch to
+ */
+struct ic_parse_table_entry {
+    unsigned int len;
+    char *token;
+    struct ic_expression * (*func)(struct ic_tokens *tokens, unsigned int *i);
+} ic_parse_table [] = {
+    {4, "type", ic_parse_type_decl},
+    {8, "function", ic_parse_func_decl}
+};
+
 void * ic_parse(struct ic_tokens *tokens){
+    /* offset into tokens */
     unsigned int i = 0;
+    /* position of next space in tokens */
     char *space = 0;
+    /* length of current token */
     unsigned int dist = 0;
+
+    /* offset into ic_parse_table */
+    unsigned int pt_offset = 0;
 
     /* possible leading tokens:
      * type
@@ -34,40 +63,40 @@ void * ic_parse(struct ic_tokens *tokens){
          * which is the distance from the start to the space
          */
         dist = space - &(tokens->tokens[i]);
-        printf("got distance %u\n", dist);
 
-        switch( dist ){
-            case 1:
-                /* # */
-                printf("1: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
-                return 0;
-                break;
+#if DEBUG_PARSE
+        printf( "considering token '%.*s' with distance '%u'\n", dist, &(tokens->tokens[i]), dist );
+#endif
 
-            case 4:
-                /* type
-                 * enum
-                 */
-                printf("4: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
-                return 0;
-                break;
+        for( pt_offset=0; pt_offset < LENGTH(ic_parse_table); ++pt_offset ){
+#if DEBUG_PARSE
+            printf( "comparing token token '%.*s' (%u) with parse_table entry '%.*s' (%u)\n",
+                    dist,
+                    &(tokens->tokens[i]),
+                    dist,
+                    ic_parse_table[pt_offset].len,
+                    ic_parse_table[pt_offset].token,
+                    ic_parse_table[pt_offset].len
+                  );
+#endif
 
-            case 5:
-                /* union */
-                printf("5: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
+            if( dist == ic_parse_table[pt_offset].len
+                && ! strncmp( &(tokens->tokens[i]), ic_parse_table[pt_offset].token, dist )
+              ){
+#if DEBUG_PARSE
+                printf( "match found! token '%.*s' with parse_table entry '%.*s'\n", dist, &(tokens->tokens[i]), ic_parse_table[pt_offset].len, ic_parse_table[pt_offset].token );
+#endif
                 return 0;
-                break;
+            }
 
-            case 8:
-                /* function */
-                printf("8: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
-                return 0;
-                break;
-
-            default:
-                printf("default: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
-                return 0;
-                break;
+            /* FIXME
+             * eventually we can rely on the parse entires being in order and bail out early
+             * for now we will just keep going
+             */
         }
+
+        /* no match found */
+        printf("ic_parse: unsupported string '%.*s' found, bailing\n", dist, &(tokens->tokens[i]) );
         return 0;
     }
 
--- src/parse.h
+++ src/parse.h
@@ -5,7 +5,9 @@
 #include "types.h"
 #include "ast.h"
 
-struct ic_type_decl * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i);
+struct ic_expression * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i);
+
+struct ic_expression * ic_parse_func_decl(struct ic_tokens *tokens, unsigned int *i);
 
 void * ic_parse(struct ic_tokens *tokens);
 
