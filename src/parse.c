d9e2dd1576017813cbc2181c830dc5da297bcc66
Chris Hall <chris.hall@catalyst.net.nz>
2015-03-11 16:41:04 +1300

    adding skeleton for ic_parse_type_decl, working on logic for ic_parse

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,9 @@
 #include "ast.h"
 #include "parse.h"
 
+#ifndef DEBUG_PARSE
 #define DEBUG_PARSE
+#endif
 
 #define LENGTH(x) (sizeof(x) / sizeof(x[0]))
 
@@ -16,22 +18,65 @@
 /* return length of token starting at source[i] */
 static unsigned int ic_parse_token_len(char *source, unsigned int i);
 
+/* advance i by one token with token length dist */
+static void ic_parse_token_advance(unsigned int *i, unsigned int dist);
+
 struct ic_decl * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i){
-    /* FIXME */
+    /* use j as internal iterator to find the end token */
+    unsigned int j = 0;
+    unsigned int dist = 0;
+
+    if( ! tokens ){
+        return 0;
+    }
+
+#ifdef DEBUG_PARSE
+    puts("ic_parse_type_decl called");
+#endif
+
+    for( j=*i; tokens->tokens[j] != '\0' && j < tokens->len ; ){
+        dist = ic_parse_token_len(tokens->tokens, j);
+        printf("ic_parse_token_type_decl: inspecting token '%.*s'\n", dist, &(tokens->tokens[j]) );
+
+        if( dist == 3 &&
+            ! strncmp( &(tokens->tokens[j]), "end", 3) ){
+            printf("ic_parse_token_type_decl: found end of string token '%.*s'\n", dist, &(tokens->tokens[j]) );
+
+            ic_parse_token_advance(&j, dist);
+            *i = j;
+            return 0;
+        }
+
+        ic_parse_token_advance(&j, dist);
+    }
+
+    /* this means we ran out of tokens */
     return 0;
 }
 
 struct ic_decl * ic_parse_enum_decl(struct ic_tokens *tokens, unsigned int *i){
+#ifdef DEBUG_PARSE
+    puts("ic_parse_enum_decl called");
+#endif
+
     /* FIXME */
     return 0;
 }
 
 struct ic_decl * ic_parse_union_decl(struct ic_tokens *tokens, unsigned int *i){
+#ifdef DEBUG_PARSE
+    puts("ic_parse_union_decl called");
+#endif
+
     /* FIXME */
     return 0;
 }
 
 struct ic_decl * ic_parse_func_decl(struct ic_tokens *tokens, unsigned int *i){
+#ifdef DEBUG_PARSE
+    puts("ic_parse_func_decl called");
+#endif
+
     /* FIXME */
     return 0;
 }
@@ -64,6 +109,9 @@ struct ic_ast * ic_parse(struct ic_tokens *tokens){
     /* function to dispatch to */
     struct ic_decl * (*func)(struct ic_tokens *tokens, unsigned int *i) = 0;
 
+    /* return from call to func */
+    struct ic_decl *ret = 0;
+
     /* possible leading tokens:
      * type
      * enum
@@ -81,6 +129,7 @@ struct ic_ast * ic_parse(struct ic_tokens *tokens){
         printf( "considering token '%.*s' with distance '%u'\n", dist, &(tokens->tokens[i]), dist );
 #endif
 
+        func = 0;
         for( pt_offset=0; pt_offset < LENGTH(ic_parse_table); ++pt_offset ){
 #ifdef DEBUG_PARSE
             printf( "comparing token token '%.*s' (%u) with parse_table entry '%.*s' (%u)\n",
@@ -110,13 +159,24 @@ struct ic_ast * ic_parse(struct ic_tokens *tokens){
                  * figure out what to do with result
                  * do that
                  */
-                return 0;
+                ret = func(tokens, &i);
+                if( ! ret ){
+                    /* presume parsing failed */
+                    puts("ic_parse: error when calling parsing function");
+                    return 0;
+                }
+                /* exit inner for loop as this token is done */
+                break;
             }
 
             /* FIXME
              * eventually we can rely on the parse entires being in order and bail out early
              * for now we will just keep going
              */
+        }
+
+        if( func ){
+            continue;
         }
 
         /* no match found */
@@ -137,6 +197,10 @@ static unsigned int ic_parse_token_len(char *source, unsigned int i){
     /* length of current token */
     unsigned int dist = 0;
 
+    if( ! source ){
+        return 0;
+    }
+
     /* find next space */
     space = strchr( &(source[i]), ' ' );
 
@@ -146,6 +210,16 @@ static unsigned int ic_parse_token_len(char *source, unsigned int i){
     dist = space - &(source[i]);
 
     return dist;
+}
+
+/* advance i by one token with token length dist */
+static void ic_parse_token_advance(unsigned int *i, unsigned int dist){
+    if( ! i ){
+        return;
+    }
+
+    /* dist + 1 to account for space */
+    *i += (dist + 1);
 }
 
 
