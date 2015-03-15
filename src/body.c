9045e04daf3236b12d3bb6c84a11840e07870796
Chris Hall <followingthepath@gmail.com>
2015-03-15 17:38:26 +1300

    adding rought ic_body

---
+++ src/body.c
@@ -0,0 +1,95 @@
+#include <stdio.h> /* puts, printf */
+#include <stdlib.h> /* calloc */
+
+#include "body.h"
+#include "pvector.h"
+
+/* allocate and initialise a new ic_body
+ *
+ * returns pointer to new ic_body on sucess
+ * returns 0 on failure
+ */
+struct ic_body * ic_body_new(void){
+    struct ic_body *body = 0;
+
+    /* alloc */
+    body = calloc(1, sizeof(struct ic_body));
+    if( ! body ){
+        puts("ic_body_new: calloc failed");
+        return 0;
+    }
+
+    /* init */
+    if( ic_body_init(body) ){
+        puts("ic_body_new: call to ic_body_init failed");
+        return 0;
+    }
+
+    return body;
+}
+
+/* initialise an existing ic_body
+ *
+ * returns 0 on sucess
+ * returns 1 on failure
+ */
+int ic_body_init(struct ic_body *body){
+    if( ! body ){
+        puts("ic_body_init: body was null");
+        return 1;
+    };
+
+    /* simply dispatch to pvector init */
+    if( ic_pvector_init( &(body->contents), 0 ) ){
+        puts("ic_body_init: call to ic_pvector_init failed");
+        return 1;
+    }
+
+    /* success */
+    return 0;
+}
+
+/* returns item at offset i on sucess
+ * returns 0 on failure
+ *
+ * FIXME return type is void * as we don't yet have a statement type
+ */
+void * ic_body_get(struct ic_body *body, unsigned int i){
+    if( ! body ){
+        puts("ic_body_get: body was null");
+        return 0;
+    };
+
+    /* let pvector do all the work */
+    return ic_pvector_get( &(body->contents), i );
+}
+
+/* append data to body
+ * FIXME data is void * as we don't yet have a statement type
+ *
+ * returns index of item on success
+ * returns -1 on failure
+ */
+int ic_body_append(struct ic_body *body, void * data){
+    if( ! body ){
+        puts("ic_body_append: body was null");
+        return -1;
+    };
+
+    /* let pvector do all the work */
+    return ic_pvector_append( &(body->contents), data );
+}
+
+/* returns length on success
+ * returns 0 on failure
+ */
+unsigned int ic_body_length(struct ic_body *body){
+    if( ! body ){
+        puts("ic_body_length: body was null");
+        return 0;
+    };
+
+    /* let pvector do all the work */
+    return ic_pvector_length( &(body->contents) );
+}
+
---
+++ src/body.h
@@ -0,0 +1,48 @@
+#ifndef ICARU_BODY_H
+#define ICARU_BODY_H
+
+#include "pvector.h"
+
+/* the body of a function or statement
+ * (such as the body of an if-statement)
+ * a body is collection of statements
+ */
+struct ic_body {
+    struct ic_pvector contents;
+};
+
+/* allocate and initialise a new ic_body
+ *
+ * returns pointer to new ic_body on sucess
+ * returns 0 on failure
+ */
+struct ic_body * ic_body_new(void);
+
+/* initialise an existing ic_body
+ *
+ * returns 0 on sucess
+ * returns 1 on failure
+ */
+int ic_body_init(struct ic_body *body);
+
+/* returns item at offset i on sucess
+ * returns 0 on failure
+ *
+ * FIXME return type is void * as we don't yet have a statement type
+ */
+void * ic_body_get(struct ic_body *body, unsigned int i);
+
+/* append data to body
+ * FIXME data is void * as we don't yet have a statement type
+ *
+ * returns index of item on success
+ * returns -1 on failure
+ */
+int ic_body_append(struct ic_body *body, void * data);
+
+/* returns length on success
+ * returns 0 on failure
+ */
+unsigned int ic_body_length(struct ic_body *body);
+
+#endif
