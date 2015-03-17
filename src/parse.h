bc94c18e9af7f33dcee24351949c755648386b49
Chris Hall <followingthepath@gmail.com>
2015-03-17 22:40:59 +1300

    documenting in parse. where implementations are

--- src/parse.h
+++ src/parse.h
@@ -10,6 +10,9 @@
  * based on their return types
  */
 
+
+/**** shared helper methods (parse.c) ****/
+
 /* returns length of token starting at source[i]
  * returns 0 on error
  */
@@ -18,7 +21,13 @@ unsigned int ic_parse_token_length(char *source, unsigned int i);
 /* advance i by one token with token length dist */
 void ic_parse_token_advance(unsigned int *i, unsigned int dist);
 
+
+/**** parse/field.c ****/
+
 struct ic_field * ic_parse_field(struct ic_tokens *tokens, unsigned int *i);
+
+
+/**** parse/decl.c ****/
 
 struct ic_decl * ic_parse_type_decl(struct ic_tokens *tokens, unsigned int *i);
 
@@ -27,6 +36,9 @@ struct ic_decl * ic_parse_enum_decl(struct ic_tokens *tokens, unsigned int *i);
 struct ic_decl * ic_parse_union_decl(struct ic_tokens *tokens, unsigned int *i);
 
 struct ic_decl * ic_parse_func_decl(struct ic_tokens *tokens, unsigned int *i);
+
+
+/**** parse.c ****/
 
 struct ic_ast * ic_parse(struct ic_tokens *tokens);
 
