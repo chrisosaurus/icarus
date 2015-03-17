e2ab37a07fb6ff76708b6eebf98e2985e888ee0a
Chris Hall <chris.hall@catalyst.net.nz>
2015-03-18 11:04:08 +1300

    adding gcov free examples to config.mk

--- config.mk
+++ config.mk
@@ -8,9 +8,13 @@ LIBS =
 
 # NB: including  -fprofile-arcs -ftest-coverage for gcov
 CFLAGS = -std=c99 -pedantic -Werror -Wall -Wextra -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wshadow -Wdeclaration-after-statement -Wunused-function -Wmaybe-uninitialized -fprofile-arcs -ftest-coverage ${INCS}
+# gcov free version
+#CFLAGS = -std=c99 -pedantic -Werror -Wall -Wextra -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wshadow -Wdeclaration-after-statement -Wunused-function -Wmaybe-uninitialized ${INCS}
 
 # NB: including  -fprofile-arcs for gcov
 LDFLAGS = -fprofile-arcs ${LIBS}
+# gcov free version
+#LDFLAGS = ${LIBS}
 
 DEBUGFLAGS = -DDEBUG_ICARUS -DDEBUG_LEXER -DDEBUG_PARSE
 
