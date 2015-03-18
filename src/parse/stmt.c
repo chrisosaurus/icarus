594b42a3cdfc138302180c11a4a9f2f8c6eebd8b
Chris Hall <chris.hall@catalyst.net.nz>
2015-03-18 17:31:57 +1300

    internal skeleton of parse/stmt.c

--- src/parse/stmt.c
+++ src/parse/stmt.c
@@ -5,6 +5,44 @@
 /* ignore unused parameter warnings */
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
+/* ignore unused functions */
+#pragma GCC diagnostic ignored "-Wunused-function"
+
+
+/* there are currently 3 types of statements
+ *  let
+ *  if
+ *  expr (void context)
+ */
+
+
+/* consume token
+ * returns ic_expr* on success
+ * returns 0 on failure
+ */
+static struct ic_stmt * ic_parse_stmt_let(struct ic_tokens *tokens, unsigned int *i){
+    puts("ic_parse_stmt_let: unimplemented");
+    return 0;
+}
+
+/* consume token
+ * returns ic_expr* on success
+ * returns 0 on failure
+ */
+static struct ic_stmt * ic_parse_stmt_if(struct ic_tokens *tokens, unsigned int *i){
+    puts("ic_parse_stmt_if: unimplemented");
+    return 0;
+}
+
+/* consume token
+ * returns ic_expr* on success
+ * returns 0 on failure
+ */
+static struct ic_stmt * ic_parse_stmt_expr(struct ic_tokens *tokens, unsigned int *i){
+    puts("ic_parse_stmt_expr: unimplemented");
+    return 0;
+}
+
 struct ic_stmt * ic_parse_stmt(struct ic_tokens *tokens, unsigned int *i){
     puts("ic_parse_stmt: unimplemented");
     return 0;
