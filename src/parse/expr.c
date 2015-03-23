bcb775cc05f44c15ee18ad1e642687b6e28c95ab
Chris Hall <followingthepath@gmail.com>
2015-03-23 22:01:31 +1300

    laying out skeleton for parsing expressions

--- src/parse/expr.c
+++ src/parse/expr.c
@@ -59,25 +59,46 @@ static struct ic_expr * ic_parse_expr_identifier(struct ic_tokens *tokens, unsig
     return 0;
 }
 
-/* consume token
+/* consume token and make a string
  * returns ic_expr* on success
  * returns 0 on failure
  */
-static struct ic_expr * ic_parse_expr_constant(struct ic_tokens *tokens, unsigned int *i){
+static struct ic_expr * ic_parse_expr_constant_string(struct ic_tokens *tokens, unsigned int *i){
     struct ic_expr * expr = 0;
 
     if( ! tokens ){
-        puts("ic_parse_expr_constant: tokens was null");
+        puts("ic_parse_expr_constant_string: tokens was null");
         return 0;
     }
     if( ! i ){
-        puts("ic_parse_expr_constant: i was null");
+        puts("ic_parse_expr_constant_string: i was null");
         return 0;
     }
 
-    puts("ic_parse_expr_constant: unimplemented");
+    puts("ic_parse_expr_constant_string: unimplemented");
     return 0;
 }
+
+/* consume token and make an int
+ * returns ic_expr* on success
+ * returns 0 on failure
+ */
+static struct ic_expr * ic_parse_expr_constant_integer(struct ic_tokens *tokens, unsigned int *i){
+    struct ic_expr * expr = 0;
+
+    if( ! tokens ){
+        puts("ic_parse_expr_constant_integer: tokens was null");
+        return 0;
+    }
+    if( ! i ){
+        puts("ic_parse_expr_constant_integer: i was null");
+        return 0;
+    }
+
+    puts("ic_parse_expr_constant_integer: unimplemented");
+    return 0;
+}
+
 
 /* consume token
  * returns ic_expr* on success
@@ -88,8 +109,119 @@ static struct ic_expr * ic_parse_expr_operator(struct ic_tokens *tokens, unsigne
     return 0;
 }
 
+/* check if the current token looks like a string
+ * returns 1 if yes
+ * returns 0 if no
+ */
+static int ic_parse_stringish(struct ic_tokens *tokens, unsigned int *i){
+    if( ! tokens ){
+        puts("ic_parse_stringish: tokens was null");
+        return 0;
+    }
+    if( ! i ){
+        puts("ic_parse_stringish: i was null");
+        return 0;
+    }
+
+    /* peek at first character and check
+     * for quotes
+     */
+    switch( tokens->tokens[*i] ){
+        case '"':
+        case '\'':
+            return 1;
+            break;
+
+        default:
+            return 0;
+            break;
+    }
+
+    return 0;
+}
+
+/* check if the current token looks like a number
+ * returns 1 if yes
+ * returns 0 if no
+ */
+static int ic_parse_numberish(struct ic_tokens *tokens, unsigned int *i){
+    if( ! tokens ){
+        puts("ic_parse_numberish: tokens was null");
+        return 0;
+    }
+    if( ! i ){
+        puts("ic_parse_numberish: i was null");
+        return 0;
+    }
+
+    /* peek at first character
+     * if this is a digit then this is a number
+     * otherwise it is not
+     */
+    switch( tokens->tokens[*i] ){
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            return 1;
+            break;
+
+        default:
+            return 0;
+            break;
+    }
+
+    return 0;
+}
+
+/* peek at token after current one
+ * returns a char* pointing to the start of the next
+ * token
+ *
+ * returns 0 on error
+ */
+static char * ic_parse_peek_next(struct ic_tokens *tokens, unsigned int *i){
+    /* dist of current token */
+    unsigned int dist = 0;
+    /* offset of next token */
+    unsigned int offset = 0;
+
+    if( ! tokens ){
+        puts("ic_parse_peek_next: tokens was null");
+        return 0;
+    }
+    if( ! i ){
+        puts("ic_parse_peek_next: i was null");
+        return 0;
+    }
+
+    dist = ic_parse_token_length(tokens->tokens, *i);
+    if( ! dist ){
+        puts("ic_parse_peek_next: call to ic_parse_token_length failed");
+        return 0;
+    }
+
+    /* plus 1 to get past space */
+    offset = dist + 1;
+
+    if( offset > tokens->len ){
+        puts("ic_parse_peek_next: no next token, out of tokens");
+        return 0;
+    }
+
+    return &(tokens->tokens[offset]);
+}
 
 struct ic_expr * ic_parse_expr(struct ic_tokens *tokens, unsigned int *i){
+    /* pointer used to peek at start of next token */
+    char *next = 0;
+
     if( ! tokens ){
         puts("ic_parse_expr: tokens was null");
         return 0;
@@ -99,8 +231,42 @@ struct ic_expr * ic_parse_expr(struct ic_tokens *tokens, unsigned int *i){
         return 0;
     }
 
+    /* rules
+     *
+     * we see a " token -> string value
+     * we see a number as the first char -> int value
+     * we see a symbol, need to inspect next token
+     *      ( -> func call
+     *      {+ - * ...} -> operator - FIXME UNSUPPORTED
+     *      else -> identifier
+     */
+    if( ic_parse_stringish(tokens, i) ){
+        return ic_parse_expr_constant_string(tokens, i);
+    }
 
-    puts("ic_parse_expr: unimplemented");
-    return 0;
+    if( ic_parse_numberish(tokens, i) ){
+        return ic_parse_expr_constant_integer(tokens, i);
+    }
+
+    next = ic_parse_peek_next(tokens, i);
+    if( ! next ){
+        /* this in theory could mean out final token is an identifier
+         * however this is illegal in practice as all expr/stmt must
+         * appears inside a body ending with `end`
+         * so this is indeed an error
+         */
+        puts("ic_parse_expr: call to ic_parse_peek_next failed");
+        return 0;
+    }
+
+    /* if we see an open bracket this is a function call */
+    if( *next == '(' ){
+        return ic_parse_expr_fcall(tokens, i);
+    }
+
+    /* FIXME no support for operations yet */
+
+    /* otherwise assume this is just an identifier */
+    return ic_parse_expr_identifier(tokens, i);
 }
 
