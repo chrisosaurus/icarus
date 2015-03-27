7e4608b6757c912040dcb53847ea9ce7f137a66a
Chris Hall <followingthepath@gmail.com>
2015-03-27 19:53:02 +1300

    merging rewrite and verify steps under new name `analyse`

---
+++ src/analyse/analyse.h
@@ -0,0 +1,9 @@
+#ifndef ICARUS_ANALYSE_H
+#define ICARUS_ANALYSE_H
+
+#include "../parse/data/ast.h"
+
+
+
+#endif
+
--- src/rewrite/rewrite.h
+++
@@ -1,9 +0,0 @@
-#ifndef ICARUS_REWRITE_H
-#define ICARUS_REWRITE_H
-
-#include "../parse/data/ast.h"
-
-
-
-#endif
-
