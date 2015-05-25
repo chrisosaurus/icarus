c293338c3797020749713fd0e8ce7460cafe68a2
Chris Hall <followingthepath@gmail.com>
2015-05-25 15:47:03 +1200

    start of new ic_type definition

---
+++ src/parse/data/type.c
@@ -0,0 +1,43 @@
+#include <stdio.h> /* puts */
+
+#include "type.h"
+
+/* ignored unusued parameter */
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+/* alloc and init a new type
+ *
+ * FIXME unknown params
+ *
+ * returns new type on success
+ * returns 0 on error
+ */
+struct ic_type * ic_type_new(void){
+    puts("ic_type_new: unimplemented");
+    return 0;
+}
+
+/* init an existing type
+ *
+ * FIXME unknown params
+ *
+ * returns 0 on success
+ * returns 1 on error
+ */
+unsigned int ic_type_init(struct ic_type *type){
+    puts("ic_type_init: unimplemented");
+    return 1;
+}
+
+/* destroy a type
+ *
+ * will only free the type if `free_type` is truthy
+ *
+ * returns 0 on success
+ * returns 1 on failure
+ */
+unsigned int ic_type_destroy(struct ic_type *type, unsigned int free_tytpe){
+    puts("ic_type_destroy: unimplemented");
+    return 1;
+}
+
---
+++ src/parse/data/type.h
@@ -0,0 +1,56 @@
+#ifndef IC_TYPE_H
+#define IC_TYPE_H
+
+#include "decl.h"
+
+/* an instance of a type
+ * ic_type has a single instance for each type within a program (FIXME consider type scoping)
+ * every reference to this type (say within an expr or as the type of a variable/value) is
+ * a type_ref instance that holds a reference to ic_type
+ *
+ * an ic_type can either be:
+ *  - a builtin - FIXME no current support for builtins
+ *  - a user defined type - ic_type_decl
+ */
+
+enum ic_type_type {
+    ic_type_builtin,
+    ic_type_user
+};
+
+struct ic_type {
+    enum ic_type_type type;
+    union {
+        /* FIXME what do we store for a builtin ? */
+        struct ic_type_decl *decl;
+    } u;
+};
+
+/* alloc and init a new type
+ *
+ * FIXME unknown params
+ *
+ * returns new type on success
+ * returns 0 on error
+ */
+struct ic_type * ic_type_new(void);
+
+/* init an existing type
+ *
+ * FIXME unknown params
+ *
+ * returns 0 on success
+ * returns 1 on error
+ */
+unsigned int ic_type_init(struct ic_type *type);
+
+/* destroy a type
+ *
+ * will only free the type if `free_type` is truthy
+ *
+ * returns 0 on success
+ * returns 1 on failure
+ */
+unsigned int ic_type_destroy(struct ic_type *type, unsigned int free_tytpe);
+
+#endif
