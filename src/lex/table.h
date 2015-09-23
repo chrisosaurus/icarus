e5862558485861c17f1cf7a8a560d95f3326895a
Chris Hall <followingthepath@gmail.com>
2015-09-23 21:09:31 +1200

    adding first pass at unused lexing table

---
+++ src/lex/table.h
@@ -0,0 +1,41 @@
+#ifndef ICARUS_TABLE_H
+#define ICARUS_TABLE_H
+
+#include "data/token.h"
+
+struct ic_table_entry {
+    char *str;
+    unsigned int len;
+    enum ic_token_id id;
+};
+
+/* a map of tokens to their ids */
+struct ic_table_entry table[] {
+    /* no entry for identifier */
+
+    /* str,         len,id */
+    { "\n",         1,  IC_NEWLINE },
+    { "end",        3,  IC_END },
+
+    { "if",         2,  IC_IF },
+    { "then",       4,  IC_THEN },
+    { "return",     6,  IC_RETURN },
+    { "let",        3,  IC_LET },
+
+    { "type",       4,  IC_TYPE },
+    { "fn",         2,  IC_FUNC },
+
+    { "->",         2,  IC_ARROW },
+    { "==",         2,  IC_EQUAL },
+    { "=",          1,  IC_ASSIGN },
+    { "::",         0,  IC_DOUBLECOLON },
+    { ".",          0,  IC_PERIOD },
+    { ",",          0,  IC_COMMA },
+
+    { "(",          0,  IC_LRBRACKET },
+    { ")",          0,  IC_RRBRACKET },
+
+    { "",           0,  IC_TOKEN_LEN }
+};
+
+#endif
