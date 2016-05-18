036a239c4c1e563709d3f9c6b9b4915a60bdca98
Chris Hall <followingthepath@gmail.com>
2016-05-18 16:51:35 +1000

    backend compile stmt and compile expr now print stmt/expr they are called on

--- src/backends/2c/expr.c
+++ src/backends/2c/expr.c
@@ -10,6 +10,11 @@
  * returns 0 on failure
  */
 unsigned int ic_b2c_compile_expr(struct ic_kludge *input_kludge, struct ic_expr *expr, FILE *out){
+  unsigned int indent_level = 1;
+
+  puts("ic_b2c_compile_expr: called for");
+  ic_expr_print(expr, &indent_level);
+
   puts("ic_b2c_compile_expr: unimplemented");
   return 0;
 }
--- src/backends/2c/stmt.c
+++ src/backends/2c/stmt.c
@@ -10,6 +10,11 @@
  * returns 0 on failure
  */
 unsigned int ic_b2c_compile_stmt(struct ic_kludge *input_kludge, struct ic_stmt *stmt, FILE *out){
+  unsigned int indent_level = 1;
+
+  puts("ic_b2c_compile_stmt: called for");
+  ic_stmt_print(stmt, &indent_level);
+
   puts("ic_b2c_compile_stmt: unimplemented");
   return 0;
 }
