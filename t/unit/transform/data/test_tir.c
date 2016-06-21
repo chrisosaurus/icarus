7ab43b24468d69c6b909d7d181a86553a56d014f
Chris Hall <followingthepath@gmail.com>
2016-06-21 22:28:03 +1000

    fixing up variable names in test_tir

--- t/unit/transform/data/test_tir.c
+++ t/unit/transform/data/test_tir.c
@@ -42,69 +42,69 @@ void basic_let_expr(void){
 }
 
 void basic_assign(void){
-  struct ic_transform_ir_assign *let_p = 0;
-  struct ic_transform_ir_assign let_l;
+  struct ic_transform_ir_assign *assign_p = 0;
+  struct ic_transform_ir_assign assign_l;
 
   puts("basic tir assign testing");
 
-  let_p = ic_transform_ir_assign_new();
-  assert( let_p );
+  assign_p = ic_transform_ir_assign_new();
+  assert( assign_p );
 
-  assert( ic_transform_ir_assign_init(&let_l) );
+  assert( ic_transform_ir_assign_init(&assign_l) );
 
-  assert( ic_transform_ir_assign_destroy( &let_l, 0 ) );
-  assert( ic_transform_ir_assign_destroy( let_p, 1 ) );
+  assert( ic_transform_ir_assign_destroy( &assign_l, 0 ) );
+  assert( ic_transform_ir_assign_destroy( assign_p, 1 ) );
 
   puts("success");
 }
 
 void basic_expr(void){
-  struct ic_transform_ir_expr *let_p = 0;
-  struct ic_transform_ir_expr let_l;
+  struct ic_transform_ir_expr *expr_p = 0;
+  struct ic_transform_ir_expr expr_l;
 
   puts("basic tir expr testing");
 
-  let_p = ic_transform_ir_expr_new();
-  assert( let_p );
+  expr_p = ic_transform_ir_expr_new();
+  assert( expr_p );
 
-  assert( ic_transform_ir_expr_init(&let_l) );
+  assert( ic_transform_ir_expr_init(&expr_l) );
 
-  assert( ic_transform_ir_expr_destroy( &let_l, 0 ) );
-  assert( ic_transform_ir_expr_destroy( let_p, 1 ) );
+  assert( ic_transform_ir_expr_destroy( &expr_l, 0 ) );
+  assert( ic_transform_ir_expr_destroy( expr_p, 1 ) );
 
   puts("success");
 }
 
 void basic_ret(void){
-  struct ic_transform_ir_ret *let_p = 0;
-  struct ic_transform_ir_ret let_l;
+  struct ic_transform_ir_ret *ret_p = 0;
+  struct ic_transform_ir_ret ret_l;
 
   puts("basic tir ret testing");
 
-  let_p = ic_transform_ir_ret_new();
-  assert( let_p );
+  ret_p = ic_transform_ir_ret_new();
+  assert( ret_p );
 
-  assert( ic_transform_ir_ret_init(&let_l) );
+  assert( ic_transform_ir_ret_init(&ret_l) );
 
-  assert( ic_transform_ir_ret_destroy( &let_l, 0 ) );
-  assert( ic_transform_ir_ret_destroy( let_p, 1 ) );
+  assert( ic_transform_ir_ret_destroy( &ret_l, 0 ) );
+  assert( ic_transform_ir_ret_destroy( ret_p, 1 ) );
 
   puts("success");
 }
 
 void basic_fcall(void){
-  struct ic_transform_ir_fcall *let_p = 0;
-  struct ic_transform_ir_fcall let_l;
+  struct ic_transform_ir_fcall *fcall_p = 0;
+  struct ic_transform_ir_fcall fcall_l;
 
   puts("basic tir fcall testing");
 
-  let_p = ic_transform_ir_fcall_new();
-  assert( let_p );
+  fcall_p = ic_transform_ir_fcall_new();
+  assert( fcall_p );
 
-  assert( ic_transform_ir_fcall_init(&let_l) );
+  assert( ic_transform_ir_fcall_init(&fcall_l) );
 
-  assert( ic_transform_ir_fcall_destroy( &let_l, 0 ) );
-  assert( ic_transform_ir_fcall_destroy( let_p, 1 ) );
+  assert( ic_transform_ir_fcall_destroy( &fcall_l, 0 ) );
+  assert( ic_transform_ir_fcall_destroy( fcall_p, 1 ) );
 
   puts("success");
 }
