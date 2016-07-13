e827e9bc49b84234c58848608283385da72f8777
Chris Hall <followingthepath@gmail.com>
2016-07-13 22:03:25 +1000

    adding skeleton of pancake bytecode

---
+++ src/backends/pancake/data/bytecode.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bytecode.h"
+
+/* allocate and initialise a new bytecode
+ *
+ * returns * on success
+ * returns 0 on failure
+ */
+struct ic_backend_pancake_bytecode * ic_backend_pancake_bytecode_new(enum ic_backend_pancake_bytecode_type type, void *arg1, void *arg2){
+  struct ic_backend_pancake_bytecode *bc = 0;
+
+  bc = calloc(1, sizeof(struct ic_backend_pancake_bytecode));
+  if (!bc) {
+    puts("ic_backend_pancake_bytecode_new: call to calloc failed");
+    return 0;
+  }
+
+  if (!ic_backend_pancake_bytecode_init(bc, type, arg1, arg2)) {
+    puts("ic_backend_pancake_bytecode_new: call to ic_backend_pancake_bytecode_init failed");
+    return 0;
+  }
+
+  return bc;
+}
+
+/* initialise an existing bytecode
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_bytecode_init(struct ic_backend_pancake_bytecode *bytecode, enum ic_backend_pancake_bytecode_type type, void *arg1, void *arg2){
+  if (!bytecode) {
+    puts("ic_backend_pancake_bytecode_init: bytecode was null");
+    return 0;
+  }
+
+  bytecode->tag = type;
+  bytecode->arg1 = arg1;
+  bytecode->arg2 = arg2;
+
+  return 1;
+}
+
+/* destroy a bytecode
+ *
+ * will only fre bytecode if `free_bytecode` is truthy
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_bytecode_destroy(struct ic_backend_pancake_bytecode *bytecode, unsigned int free_bytecode){
+  if (!bytecode) {
+    puts("ic_backend_pancake_bytecode_destroy: bytecode was null");
+    return 0;
+  }
+
+  if (free_bytecode) {
+    free(bytecode);
+  }
+
+  return 1;
+}
+
---
+++ src/backends/pancake/data/bytecode.h
@@ -0,0 +1,56 @@
+#ifndef IC_BACKEND_PANCAKE_BYTECODE_H
+#define IC_BACKEND_PANCAKE_BYTECODE_H
+
+enum ic_backend_pancake_bytecode_type {
+  /* push_int int */
+  ibpb_pushint,
+  /* push_str string */
+  ibpb_pushstr,
+  /* push key */
+  ibpb_push,
+  /* call fname argn */
+  ibpb_call,
+  /* pop n */
+  ibpb_pop,
+  /* return */
+  ibpb_return,
+  /* jmp addr */
+  ibpb_jmp,
+  /* jif addr */
+  ibpb_jif,
+  /* jnif addr */
+  ibpb_jnif,
+  /* panic str */
+  ibpb_panic,
+};
+
+struct ic_backend_pancake_bytecode {
+  enum ic_backend_pancake_bytecode_type tag;
+  void *arg1;
+  void *arg2;
+};
+
+/* allocate and initialise a new bytecode
+ *
+ * returns * on success
+ * returns 0 on failure
+ */
+struct ic_backend_pancake_bytecode * ic_backend_pancake_bytecode_new(enum ic_backend_pancake_bytecode_type type, void *arg1, void *arg2);
+
+/* initialise an existing bytecode
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_bytecode_init(struct ic_backend_pancake_bytecode *bytecode, enum ic_backend_pancake_bytecode_type type, void *arg1, void *arg2);
+
+/* destroy a bytecode
+ *
+ * will only fre bytecode if `free_bytecode` is truthy
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_bytecode_destroy(struct ic_backend_pancake_bytecode *bytecode, unsigned int free_bytecode);
+
+#endif /* IC_BACKEND_PANCAKE_BYTECODE_H */
