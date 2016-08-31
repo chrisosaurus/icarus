fb603ea265c3746564c99cd4610349c02c1945ba
Chris Hall <followingthepath@gmail.com>
2016-08-31 21:28:33 +1000

    pancake: builtins adding println_ for {bool, uint, sint}

--- src/backends/pancake/runtime/builtins.c
+++ src/backends/pancake/runtime/builtins.c
@@ -1,9 +1,46 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "builtins.h"
 
-#define ic_backend_pancake_builtins_table_len 1
+/* print a string followed by a \n
+ *
+ * pops string from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake_value_stack *value_stack);
+
+/* print a sint followed by a \n
+ *
+ * pops sint from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_sint(struct ic_backend_pancake_value_stack *value_stack);
+
+/* print a uint followed by a \n
+ *
+ * pops uint from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_uint(struct ic_backend_pancake_value_stack *value_stack);
+
+/* print a bool followed by a \n
+ *
+ * pops bool from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_bool(struct ic_backend_pancake_value_stack *value_stack);
+
+#define ic_backend_pancake_builtins_table_len 4
 
 /* table mapping user-land names to internal names */
 struct ic_backend_pancake_builtins_table_type {
@@ -11,6 +48,9 @@ struct ic_backend_pancake_builtins_table_type {
     unsigned int (*func)(struct ic_backend_pancake_value_stack *value_stack);
 } ic_backend_pancake_builtins_table[ic_backend_pancake_builtins_table_len] = {
     {"println(String)", ic_backend_pancake_builtin_println_string},
+    {"println(Uint)", ic_backend_pancake_builtin_println_uint},
+    {"println(Sint)", ic_backend_pancake_builtin_println_sint},
+    {"println(Bool)", ic_backend_pancake_builtin_println_bool},
 };
 
 /* get builtin function for user-land name
@@ -81,5 +121,128 @@ unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake
     str = value->u.string;
 
     puts(str);
+    return 1;
+}
+
+/* print a uint followed by a \n
+ *
+ * pops uint from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_uint(struct ic_backend_pancake_value_stack *value_stack) {
+    struct ic_backend_pancake_value *value = 0;
+    unsigned int uint = 0;
+
+    if (!value_stack) {
+        puts("ic_backend_pancake_builtin_println_uint: value_stack was null");
+        return 0;
+    }
+
+    value = ic_backend_pancake_value_stack_peek(value_stack);
+
+    if (!value) {
+        puts("ic_backend_pancake_builtin_println_uint: call to ic_backend_pancake_value_stack_peek failed");
+        return 0;
+    }
+
+    if (value->tag != ic_backend_pancake_value_type_uint) {
+        puts("ic_backend_pancake_builtin_println_uint: value was not of expected type uint");
+        fputs("found: ", stdout);
+        ic_backend_pancake_value_print(value, stdout);
+        return 0;
+    }
+
+    if (!ic_backend_pancake_value_stack_pop(value_stack)) {
+        puts("ic_backend_pancake_builtin_println_uint: call to ic_backend_pancake_value_stack_pop failed");
+        return 0;
+    }
+
+    uint = value->u.uint;
+
+    printf("%u\n", uint);
+    return 1;
+}
+
+/* print a sint followed by a \n
+ *
+ * pops sint from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_sint(struct ic_backend_pancake_value_stack *value_stack) {
+    struct ic_backend_pancake_value *value = 0;
+    int sint = 0;
+
+    if (!value_stack) {
+        puts("ic_backend_pancake_builtin_println_string: value_stack was null");
+        return 0;
+    }
+
+    value = ic_backend_pancake_value_stack_peek(value_stack);
+
+    if (!value) {
+        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_peek failed");
+        return 0;
+    }
+
+    if (value->tag != ic_backend_pancake_value_type_sint) {
+        puts("ic_backend_pancake_builtin_println_string: value was not of expected type int");
+        fputs("found: ", stdout);
+        ic_backend_pancake_value_print(value, stdout);
+        return 0;
+    }
+
+    if (!ic_backend_pancake_value_stack_pop(value_stack)) {
+        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_pop failed");
+        return 0;
+    }
+
+    sint = value->u.sint;
+
+    printf("%d\n", sint);
+    return 1;
+}
+
+/* print a bool followed by a \n
+ *
+ * pops bool from value_stack
+ *
+ * returns 1 on success
+ * returns 0 on failure
+ */
+unsigned int ic_backend_pancake_builtin_println_bool(struct ic_backend_pancake_value_stack *value_stack) {
+    struct ic_backend_pancake_value *value = 0;
+    bool boolean = 0;
+
+    if (!value_stack) {
+        puts("ic_backend_pancake_builtin_println_string: value_stack was null");
+        return 0;
+    }
+
+    value = ic_backend_pancake_value_stack_peek(value_stack);
+
+    if (!value) {
+        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_peek failed");
+        return 0;
+    }
+
+    if (value->tag != ic_backend_pancake_value_type_bool) {
+        puts("ic_backend_pancake_builtin_println_string: value was not of expected type bool");
+        fputs("found: ", stdout);
+        ic_backend_pancake_value_print(value, stdout);
+        return 0;
+    }
+
+    if (!ic_backend_pancake_value_stack_pop(value_stack)) {
+        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_pop failed");
+        return 0;
+    }
+
+    boolean = value->u.boolean;
+
+    printf("%d\n", boolean);
     return 1;
 }
--- src/backends/pancake/runtime/builtins.h
+++ src/backends/pancake/runtime/builtins.h
@@ -3,15 +3,6 @@
 
 #include "../data/value_stack.h"
 
-/* print a string followed by a \n
- *
- * pops string from value_stack
- *
- * returns 1 on success
- * returns 0 on failure
- */
-unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake_value_stack *value_stack);
-
 typedef unsigned int (*ic_backend_function_sig)(struct ic_backend_pancake_value_stack *);
 
 /* get builtin function for user-land name
