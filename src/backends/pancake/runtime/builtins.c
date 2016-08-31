c57c689609cfd7ee544609bf0d713162a7f88037
Chris Hall <followingthepath@gmail.com>
2016-08-31 22:07:26 +1000

    tidying up pancake builtins

--- src/backends/pancake/runtime/builtins.c
+++ src/backends/pancake/runtime/builtins.c
@@ -4,41 +4,13 @@
 
 #include "builtins.h"
 
-/* print a string followed by a \n
- *
- * pops string from value_stack
- *
- * returns 1 on success
- * returns 0 on failure
- */
-unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake_value_stack *value_stack);
+unsigned int i_println_string(struct ic_backend_pancake_value_stack *value_stack);
 
-/* print a sint followed by a \n
- *
- * pops sint from value_stack
- *
- * returns 1 on success
- * returns 0 on failure
- */
-unsigned int ic_backend_pancake_builtin_println_sint(struct ic_backend_pancake_value_stack *value_stack);
+unsigned int i_println_sint(struct ic_backend_pancake_value_stack *value_stack);
 
-/* print a uint followed by a \n
- *
- * pops uint from value_stack
- *
- * returns 1 on success
- * returns 0 on failure
- */
-unsigned int ic_backend_pancake_builtin_println_uint(struct ic_backend_pancake_value_stack *value_stack);
+unsigned int i_println_uint(struct ic_backend_pancake_value_stack *value_stack);
 
-/* print a bool followed by a \n
- *
- * pops bool from value_stack
- *
- * returns 1 on success
- * returns 0 on failure
- */
-unsigned int ic_backend_pancake_builtin_println_bool(struct ic_backend_pancake_value_stack *value_stack);
+unsigned int i_println_bool(struct ic_backend_pancake_value_stack *value_stack);
 
 #define ic_backend_pancake_builtins_table_len 4
 
@@ -47,10 +19,10 @@ struct ic_backend_pancake_builtins_table_type {
     char *str;
     unsigned int (*func)(struct ic_backend_pancake_value_stack *value_stack);
 } ic_backend_pancake_builtins_table[ic_backend_pancake_builtins_table_len] = {
-    {"println(String)", ic_backend_pancake_builtin_println_string},
-    {"println(Uint)", ic_backend_pancake_builtin_println_uint},
-    {"println(Sint)", ic_backend_pancake_builtin_println_sint},
-    {"println(Bool)", ic_backend_pancake_builtin_println_bool},
+    {"println(String)", i_println_string},
+    {"println(Uint)", i_println_uint},
+    {"println(Sint)", i_println_sint},
+    {"println(Bool)", i_println_bool},
 };
 
 /* get builtin function for user-land name
@@ -90,31 +62,31 @@ ic_backend_function_sig ic_backend_pancake_builtins_table_get(char *str) {
  * returns 1 on success
  * returns 0 on failure
  */
-unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake_value_stack *value_stack) {
+unsigned int i_println_string(struct ic_backend_pancake_value_stack *value_stack) {
     struct ic_backend_pancake_value *value = 0;
     char *str = 0;
 
     if (!value_stack) {
-        puts("ic_backend_pancake_builtin_println_string: value_stack was null");
+        puts("i_println_string: value_stack was null");
         return 0;
     }
 
     value = ic_backend_pancake_value_stack_peek(value_stack);
 
     if (!value) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_peek failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_peek failed");
         return 0;
     }
 
     if (value->tag != ic_backend_pancake_value_type_string) {
-        puts("ic_backend_pancake_builtin_println_string: value was not of expected type string");
+        puts("i_println_string: value was not of expected type string");
         fputs("found: ", stdout);
         ic_backend_pancake_value_print(value, stdout);
         return 0;
     }
 
     if (!ic_backend_pancake_value_stack_pop(value_stack)) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_pop failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_pop failed");
         return 0;
     }
 
@@ -131,31 +103,31 @@ unsigned int ic_backend_pancake_builtin_println_string(struct ic_backend_pancake
  * returns 1 on success
  * returns 0 on failure
  */
-unsigned int ic_backend_pancake_builtin_println_uint(struct ic_backend_pancake_value_stack *value_stack) {
+unsigned int i_println_uint(struct ic_backend_pancake_value_stack *value_stack) {
     struct ic_backend_pancake_value *value = 0;
     unsigned int uint = 0;
 
     if (!value_stack) {
-        puts("ic_backend_pancake_builtin_println_uint: value_stack was null");
+        puts("i_println_uint: value_stack was null");
         return 0;
     }
 
     value = ic_backend_pancake_value_stack_peek(value_stack);
 
     if (!value) {
-        puts("ic_backend_pancake_builtin_println_uint: call to ic_backend_pancake_value_stack_peek failed");
+        puts("i_println_uint: call to ic_backend_pancake_value_stack_peek failed");
         return 0;
     }
 
     if (value->tag != ic_backend_pancake_value_type_uint) {
-        puts("ic_backend_pancake_builtin_println_uint: value was not of expected type uint");
+        puts("i_println_uint: value was not of expected type uint");
         fputs("found: ", stdout);
         ic_backend_pancake_value_print(value, stdout);
         return 0;
     }
 
     if (!ic_backend_pancake_value_stack_pop(value_stack)) {
-        puts("ic_backend_pancake_builtin_println_uint: call to ic_backend_pancake_value_stack_pop failed");
+        puts("i_println_uint: call to ic_backend_pancake_value_stack_pop failed");
         return 0;
     }
 
@@ -172,31 +144,31 @@ unsigned int ic_backend_pancake_builtin_println_uint(struct ic_backend_pancake_v
  * returns 1 on success
  * returns 0 on failure
  */
-unsigned int ic_backend_pancake_builtin_println_sint(struct ic_backend_pancake_value_stack *value_stack) {
+unsigned int i_println_sint(struct ic_backend_pancake_value_stack *value_stack) {
     struct ic_backend_pancake_value *value = 0;
     int sint = 0;
 
     if (!value_stack) {
-        puts("ic_backend_pancake_builtin_println_string: value_stack was null");
+        puts("i_println_string: value_stack was null");
         return 0;
     }
 
     value = ic_backend_pancake_value_stack_peek(value_stack);
 
     if (!value) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_peek failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_peek failed");
         return 0;
     }
 
     if (value->tag != ic_backend_pancake_value_type_sint) {
-        puts("ic_backend_pancake_builtin_println_string: value was not of expected type int");
+        puts("i_println_string: value was not of expected type int");
         fputs("found: ", stdout);
         ic_backend_pancake_value_print(value, stdout);
         return 0;
     }
 
     if (!ic_backend_pancake_value_stack_pop(value_stack)) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_pop failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_pop failed");
         return 0;
     }
 
@@ -213,31 +185,31 @@ unsigned int ic_backend_pancake_builtin_println_sint(struct ic_backend_pancake_v
  * returns 1 on success
  * returns 0 on failure
  */
-unsigned int ic_backend_pancake_builtin_println_bool(struct ic_backend_pancake_value_stack *value_stack) {
+unsigned int i_println_bool(struct ic_backend_pancake_value_stack *value_stack) {
     struct ic_backend_pancake_value *value = 0;
     bool boolean = 0;
 
     if (!value_stack) {
-        puts("ic_backend_pancake_builtin_println_string: value_stack was null");
+        puts("i_println_string: value_stack was null");
         return 0;
     }
 
     value = ic_backend_pancake_value_stack_peek(value_stack);
 
     if (!value) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_peek failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_peek failed");
         return 0;
     }
 
     if (value->tag != ic_backend_pancake_value_type_bool) {
-        puts("ic_backend_pancake_builtin_println_string: value was not of expected type bool");
+        puts("i_println_string: value was not of expected type bool");
         fputs("found: ", stdout);
         ic_backend_pancake_value_print(value, stdout);
         return 0;
     }
 
     if (!ic_backend_pancake_value_stack_pop(value_stack)) {
-        puts("ic_backend_pancake_builtin_println_string: call to ic_backend_pancake_value_stack_pop failed");
+        puts("i_println_string: call to ic_backend_pancake_value_stack_pop failed");
         return 0;
     }
 
